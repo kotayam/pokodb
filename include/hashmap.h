bcc5986679516d2fe4dfe6350f4e78b53bf59650
Kota Yamamoto <ykomako4.9@gmail.com>
2024-11-24 23:31:53 +0900

    define header for hashmap

---
+++ include/hashmap.h
@@ -0,0 +1,27 @@
+#ifndef HASHMAP_H
+#define HASHMAP_H
+
+#define TABLE_SIZE 100
+
+typedef struct key_value_t {
+    char* key;
+    char* value;
+    key_value* next;
+} key_value;
+
+typedef struct hashmap_t {
+    key_value** entries;
+    int size;
+} hashmap;
+
+void constructor();
+
+void hash();
+
+void insert();
+
+void get();
+
+void update();
+
+#endif
