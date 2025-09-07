20f8f765a4d6bb2ce82da6afbcf4a0845286c2be
kotayam <ykomako4.9@gmail.com>
2025-09-07 15:57:42 -0400

    update import

--- node_api/lib/binding.js
+++ node_api/lib/binding.js
@@ -1,27 +1,27 @@
-const addon = require("../build/Release/pokodb-native");
+const addon = require("../../build/Release/pokodb-native");
 
 function Pokodb() {
-  this.insert = function (key, value) {
-    return _addonInstance.insert(key, value);
-  };
+    this.insert = function(key, value) {
+        return _addonInstance.insert(key, value);
+    };
 
-  this.get = function (key) {
-    return _addonInstance.get(key);
-  };
+    this.get = function(key) {
+        return _addonInstance.get(key);
+    };
 
-  this.delete = function (key) {
-    return _addonInstance.delete(key);
-  };
+    this.delete = function(key) {
+        return _addonInstance.delete(key);
+    };
 
-  this.update = function (key, value) {
-    return _addonInstance.update(key, value);
-  };
+    this.update = function(key, value) {
+        return _addonInstance.update(key, value);
+    };
 
-  this.close = function () {
-    return _addonInstance.close();
-  };
+    this.close = function() {
+        return _addonInstance.close();
+    };
 
-  var _addonInstance = new addon.Pokodb();
+    var _addonInstance = new addon.Pokodb();
 }
 
 module.exports = Pokodb;
